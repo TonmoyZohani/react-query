7539ced2a30f11074fd5acc759389addd442e30d
TonmoyZohani <tonmoyzohani@gmail.com>
2024-06-05 22:18:18 +0600

    fetch by usequery

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,9 +1,16 @@
-import { useQueryClient } from '@tanstack/react-query'
+import { useQuery, useQueryClient } from '@tanstack/react-query'
 import React from 'react'
 
 const Test = () => {
 
-  const queryClient = useQueryClient
+  const queryClient = useQueryClient();
+
+  const {data,error,isLoading} = useQuery({
+    queryKey:["posts"],
+    queryFn:()=>{
+      return fetch(`https://jsonplaceholder.typicode.com/posts`).then((res)=>res.json)
+    }
+  })
 
   return (
     <div>Test</div>
